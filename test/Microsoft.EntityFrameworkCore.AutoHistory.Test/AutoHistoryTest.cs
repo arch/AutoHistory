e97f6acfa5a5a2357ac1e97ac80c8139ca530345
RigoFunc <rigofunc@outlook.com>
2016-10-17 18:47:11 +0800

    feat: add init unit test

---
+++ test/Microsoft.EntityFrameworkCore.AutoHistory.Test/AutoHistoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore.AutoHistory.Test {
+    public class AutoHistoryTest {
+        [Fact]
+        public void Entity_Add_AutoHistory_Test() {
+            using (var db = new BloggingContext()) {
+                db.Blogs.Add(new Blog {
+                    Url = "http://blogs.msdn.com/adonet",
+                    Posts = new List<Post> {
+                        new Post {
+                            Title = "xUnit",
+                            Content = "Post from xUnit test."
+                        }
+                    }
+                });
+
+                db.EnsureAutoHistory();
+
+                var count = db.ChangeTracker.Entries().Count(e => e.State == EntityState.Added);
+
+                Assert.Equal(4, count);
+            }
+        }
+    }
+}
