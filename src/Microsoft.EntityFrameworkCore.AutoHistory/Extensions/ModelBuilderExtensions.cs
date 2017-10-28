e8a00f70f858502dc665c2cf953643cbe98515a4
xyting <rigofunc@outlook.com>
2017-10-28 14:40:20 +0800

    fix: fix the binary back compatibility issues

--- src/Microsoft.EntityFrameworkCore.AutoHistory/Extensions/ModelBuilderExtensions.cs
+++ src/Microsoft.EntityFrameworkCore.AutoHistory/Extensions/ModelBuilderExtensions.cs
@@ -7,29 +7,27 @@ namespace Microsoft.EntityFrameworkCore
     /// </summary>
     public static class ModelBuilderExtensions
     {
+        private const int DefaultChangedMaxLength = 2048;
+
         /// <summary>
         /// Enables the automatic recording change history.
         /// </summary>
-        /// <param name="modelBuilder">The <see cref="ModelBuilder"/> to enable auto history functionality.</param>
-        /// <param name="changedMaxLength">The maximum length of the 'Changed' column. <c>null</c> to remove the max length restriction and will use EF Core default setting.</param>
+        /// <param name="modelBuilder">The <see cref="ModelBuilder"/> to enable auto history feature.</param>
+        /// <param name="changedMaxLength">The maximum length of the 'Changed' column. <c>null</c> will use default setting 2048.</param>
         /// <returns>The <see cref="ModelBuilder"/> had enabled auto history feature.</returns>
-        public static ModelBuilder EnableAutoHistory(this ModelBuilder modelBuilder, int? changedMaxLength = 2048)
+        public static ModelBuilder EnableAutoHistory(this ModelBuilder modelBuilder, int? changedMaxLength)
         {
             modelBuilder.Entity<AutoHistory>(b =>
             {
                 b.Property(c => c.RowId).IsRequired().HasMaxLength(50);
                 b.Property(c => c.TableName).IsRequired().HasMaxLength(128);
-                var changedProperty = b.Property(c => c.Changed);
-                if (changedMaxLength.HasValue)
-                {
-                    var max = changedMaxLength.Value;
-                    if(max <= 0)
-                    {
-                        max = 2048;    
-                    }
 
-                    changedProperty.HasMaxLength(max);
+                var max = changedMaxLength ?? DefaultChangedMaxLength;
+                if (max <= 0)
+                {
+                    max = DefaultChangedMaxLength;
                 }
+                b.Property(c => c.Changed).HasMaxLength(max);
 
                 // This MSSQL only
                 //b.Property(c => c.Created).HasDefaultValueSql("getdate()");
